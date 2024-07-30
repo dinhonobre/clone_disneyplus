98bdf273604e6e10bb763c2ee69431990acd896a
anderson <nobreanderson@hotmail.com>
2024-07-30 18:39:02 -0300

    commit two

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-const gulp = require('gulp');
+const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin')
 
 
 function styles () {
@@ -8,7 +9,14 @@ function styles () {
     .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
-exports.watch = function(){
+function images() {
+    return gulp.src('./src/src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/src/images'));
+}
+
+exports.default = gulp.parallel(styles, images);
+
+exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
