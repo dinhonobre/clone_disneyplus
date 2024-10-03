302a8b0c7eaa1571f32b6bd45e3da40baf378166
anderson <nobreanderson@hotmail.com>
2024-10-03 19:42:57 -0300

    section FAQ

---
+++ src/main.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('[data-tab-button]'); 
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(event) {
+            const abaAlvo = event.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`);
+            escondeTodasAbas();
+            aba.classList.add('shows__list--is-active');
+            removeBotaoAtivo();
+            event.target.classList.add('shows__tabs__button--is-active');
+        });
+    }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
+})
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+
+}
+
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-tab-button]'); 
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active');
+    }
+}
+
+function escondeTodasAbas() {
+    const tabContainer = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabContainer.length; i++) {
+        tabContainer[i].classList.remove('shows__list--is-active');
+    }
+}
