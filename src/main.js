88551131c2a0da58ac04349e38366a5c5e52fe75
anderson <nobreanderson@hotmail.com>
2024-10-07 19:52:57 -0300

    Resolvendo conflitos e atualizando a main

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]'); 
-<<<<<<< HEAD
-=======
     const questions = document.querySelectorAll('[data-faq-question]');
->>>>>>> master
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(event) {
@@ -15,14 +12,11 @@ document.addEventListener('DOMContentLoaded', function() {
             event.target.classList.add('shows__tabs__button--is-active');
         });
     }
-<<<<<<< HEAD
 });
-=======
 
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
-})
 
 function abreOuFechaResposta(elemento) {
     const classe = 'faq__questions__item--is-open';
@@ -31,7 +25,6 @@ function abreOuFechaResposta(elemento) {
     elementoPai.classList.toggle(classe);
 
 }
->>>>>>> master
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]'); 
